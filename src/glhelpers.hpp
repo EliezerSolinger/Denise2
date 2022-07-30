ad572a8a3f70b23693e30057701c0219f2e93db5
Eliezer Solinger <eliezersolinger@gmail.com>
2022-07-30 17:44:57 -0300

    ajustando cubemaps 2

--- src/glhelpers.hpp
+++ src/glhelpers.hpp
@@ -4,6 +4,7 @@
 #include "dmath.hpp"
 
 using DMath::Color;
+using DMath::ByteColor;
 using DMath::Vec3;
 using DMath::Vec2;
 using DMath::Mat4;
@@ -30,6 +31,8 @@ using DMath::BoundBox;
 #include <map>
 #include <string> 
 #include <cstring>
+#include <ostream>
+#include <iostream>
 
 namespace DGL {
     inline void flush() {glFlush();}
@@ -62,36 +65,150 @@ namespace DGL {
     inline void viewport(GLint x, GLint y, GLsizei width, GLsizei height) { glViewport(x, y, width, height); }
     
 
+    
+    class CPUTextureBuffer {
+        public:
+            uint8_t *buffer=NULL;
+            size_t buffer_size;
+            uint32_t width=0;
+            uint32_t height=0;
+            uint32_t channels=1;
+            
+        static inline CPUTextureBuffer FROM_POINTER(uint32_t width, uint32_t height, uint32_t channels,void *pointer) {
+          CPUTextureBuffer t(width,height,channels);
+          t.buffer=(uint8_t*)pointer;
+          return t;
+        }
+
+        inline size_t get_pixel_index(uint32_t x,uint32_t y,uint32_t channel=0) {
+            if(x>width) { err_printf("ERRO, acessando pixel além da largura \n");  return 0; }
+            if(y>height) { err_printf("ERRO, acessando pixel além da altura \n");  return 0;  }
+            auto i=((y * width + x)*channels)+channel;
+            if(i>buffer_size) {
+                err_printf("ERRO, pesquisando indice de pixel além do limite do array \n");  return 0; 
+            }
+            return i;
+
+        }
+        inline Color get_pixel_color(uint32_t x,uint32_t y) { 
+            size_t i=get_pixel_index(x,y);
+            uint8_t alpha=255;
+            if(channels==4) alpha=buffer[i+3]; 
+            return Color::FROM_BYTE_COMPONENTS(buffer[i+0],buffer[i+1],buffer[i+2],alpha);
+        }
+        inline void put_pixel_color(uint32_t x,uint32_t y,Color color) {
+            size_t i=get_pixel_index(x,y);
+            auto c=color.to_bytes();
+            buffer[i+0]=c.r; buffer[i+1]=c.g; buffer[i+2]=c.b;
+            if(channels==4) buffer[i+3]=c.a;
+        }
+        inline ByteColor get_pixel(uint32_t x,uint32_t y) { 
+            size_t i=get_pixel_index(x,y);
+            return ByteColor(buffer[i+0],buffer[i+1],buffer[i+2],(channels==4 ? buffer[i+3] : 255));
+        }
+        inline void put_pixel(uint32_t x,uint32_t y,ByteColor c) {
+            size_t i=get_pixel_index(x,y);
+            buffer[i+0]=c.r;
+            buffer[i+1]=c.g;
+            buffer[i+2]=c.b;
+            if(channels==4) buffer[i+3]=c.a;
+        }
+        inline void put_pixel_color(uint32_t x,uint32_t y,uint8_t v) { 
+            buffer[get_pixel_index(x,y)]=v;
+        }
+        inline void free() {
+            if(buffer) delete [] buffer;
+            buffer=NULL;
+        }
+        CPUTextureBuffer() { }
+        CPUTextureBuffer(uint32_t width,uint32_t height,uint32_t channels=4) {
+            this->width=width;
+            this->height=height;
+            this->channels=channels;
+            this->buffer_size=width*height*channels;
+        }
+        inline void alloc() { 
+            buffer_size=width*height*channels;
+            buffer=new uint8_t[buffer_size];
+            memset(buffer,255,buffer_size);
+        }
+        inline CPUTextureBuffer extract_quad(uint32_t x,uint32_t y,uint32_t w,uint32_t h) {
+            CPUTextureBuffer quad(w,h,channels);
+            quad.alloc();
+            for(uint32_t i=0;i<w;i++) {
+                for(uint32_t j=0;j<h;j++) { 
+                    quad.put_pixel(i,j,get_pixel(x+(i%width),y+(j%height)));
+                }
+            }
+            return quad;
+        }
+        inline CPUTextureBuffer resized(uint32_t w,uint32_t h,uint32_t channels=4) {
+            auto img=CPUTextureBuffer(w,h,channels);
+            img.alloc();
+            auto img_factor=Vec2(1.0,1.0)/Vec2(w,h); 
+            for(uint32_t y=0;y<h;y++) { 
+                for(uint32_t x=0;x<w;x++) {
+                    auto pos=Vec2(x,y)*img_factor*Vec2(width,height);
+                    img.put_pixel(x,y,// da pra colocar uma interpolacao aqui mas nao to afim87
+                        get_pixel(pos.x,pos.y)
+                    );
+                }
+            }
+            return img;
+        }
+       
+
+   };
+
+
     class Texture {
         public:
         unsigned int id=0;
+        bool is_cubemap=false;
         Texture(unsigned int id) {this->id=id;}
         Texture() { id=0; }
 
-        inline void bind(GLuint texture_unit=GL_TEXTURE0,bool cubemap=false) {
+        inline void bind(GLuint texture_unit=0) {
             if(!id) {  err_printf("trying to bind a null texture\n");   }
             glActiveTexture(GL_TEXTURE0+texture_unit);
-            if(cubemap) glBindTexture(GL_TEXTURE_CUBE_MAP, id);
+            if(is_cubemap) glBindTexture(GL_TEXTURE_CUBE_MAP, id);
             else glBindTexture(GL_TEXTURE_2D, id);
         }
-        inline void unbind() { glBindTexture(GL_TEXTURE_2D, 0); }
+        inline void unbind() { 
+            if(is_cubemap) glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+            else glBindTexture(GL_TEXTURE_2D, 0);
+        }
         inline void configure(bool smoth=false,bool mipmap=false, bool clamp=false) {
             bind();
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, clamp ? GL_CLAMP_TO_EDGE : GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, clamp ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+            auto target= is_cubemap ? GL_TEXTURE_CUBE_MAP : GL_TEXTURE_2D;
+         
+            glTexParameteri(target, GL_TEXTURE_WRAP_S, clamp ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+            glTexParameteri(target, GL_TEXTURE_WRAP_T, clamp ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+            //if(is_cubemap) glTexParameteri(target, GL_TEXTURE_WRAP_R, clamp ? GL_CLAMP_TO_EDGE : GL_REPEAT);
         
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, smoth ? GL_LINEAR  : GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, smoth ? GL_LINEAR : GL_NEAREST); 
-            if(mipmap) glGenerateMipmap(GL_TEXTURE_2D);
-        }
+            glTexParameteri(target, GL_TEXTURE_MIN_FILTER, smoth ? GL_LINEAR  : GL_NEAREST);
+            glTexParameteri(target, GL_TEXTURE_MAG_FILTER, smoth ? GL_LINEAR : GL_NEAREST); 
+            if(mipmap) glGenerateMipmap(target);
+        
+        } 
         inline bool is_texture() { return id!=0 || glIsTexture( id); }
         inline void set_image(const GLvoid *data,GLsizei width,GLsizei height,GLuint channels_format=GL_RGB,GLuint byte_format=GL_UNSIGNED_BYTE)  {
             log_printf("Loading texture id %d with %d pixels\n",id,width*height);
             glTexImage2D(GL_TEXTURE_2D, 0, channels_format, width, height, 0, channels_format, byte_format, data);
         }
+        inline void set_cubemap_image(GLuint face,const GLvoid *data,GLsizei width,GLsizei height,GLuint channels_format=GL_RGB,GLuint byte_format=GL_UNSIGNED_BYTE) {
+            
+             glTexImage2D(face, 0, channels_format, width, height, 0, channels_format, byte_format, data);
+
+        }
         inline void create() {
             glGenTextures(1, &id);
             log_printf("Texture id: %d created\n",id);
+        }
+        inline void create_cubemap() {
+            glGenTextures(1, &id);
+            log_printf("Cubemap texture id: %d created\n",id);
+            is_cubemap=true;
         }
         inline void delete_texture() {
             if(!is_texture()) {
@@ -117,6 +234,63 @@ namespace DGL {
             uint8_t data[4]={255,255,255};
             return create_texture_from_array(data, 1, 1);
         }
+        static inline Texture LOAD_FROM_BUFFER(CPUTextureBuffer buffer,bool smoth=false,bool mipmap=false, bool clamp=false) { 
+            log_printf("Sending CPUTextureBuffer to gpu...\n");
+            GLuint channels_format=GL_LUMINANCE;
+            if(buffer.channels==3) channels_format=GL_RGB;
+            if(buffer.channels==4) channels_format=GL_RGBA;
+
+            Texture texture;
+            texture.create();
+            texture.bind();
+            texture.set_image(buffer.buffer,buffer.width,buffer.height,channels_format,GL_UNSIGNED_BYTE);
+            texture.configure(smoth,mipmap,clamp);
+            texture.unbind();
+            return texture;
+        } 
+        static inline Texture LOAD_CUBEMAP_FROM_BUFFER(CPUTextureBuffer buffer,bool smoth=true,bool mipmap=false, bool clamp=false) {
+            log_printf("Sending CPUTextureBuffer to gpu as cubemap...\n");
+            GLuint channels_format=GL_LUMINANCE;
+            if(buffer.channels==3) channels_format=GL_RGB;
+            if(buffer.channels==4) channels_format=GL_RGBA;
+            auto factor=Vec2((float)buffer.width/4.0f,(float)buffer.height/3.0f);
+            
+            auto get_part=[&](int x,int y) { 
+                auto face=buffer.extract_quad(
+                    (uint32_t) x*factor.x, (uint32_t) y*factor.y,
+                    (uint32_t)factor.x,(uint32_t)factor.y
+                );
+                uint32_t big_res= factor.x>factor.y ? factor.x : factor.y;
+                auto resized=face.resized(big_res,big_res,face.channels);
+                face.free();
+                return resized; 
+            };
+            std::map<uint32_t,DMath::Vec2i> coordinates={
+                {GL_TEXTURE_CUBE_MAP_NEGATIVE_X,{0,1}},
+                {GL_TEXTURE_CUBE_MAP_POSITIVE_X,{3,1}},
+                {GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,{1,2}},
+                {GL_TEXTURE_CUBE_MAP_POSITIVE_Y,{1,0}},
+                {GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,{1,1}},
+                {GL_TEXTURE_CUBE_MAP_POSITIVE_Z,{1,2}},
+            };
+            Texture texture;
+            texture.create_cubemap();
+            texture.bind();
+            for(auto& c : coordinates) {
+                
+                auto buffer=get_part(c.second.x,c.second.y);
+
+                texture.set_cubemap_image(c.first,buffer.buffer,buffer.width,buffer.height,channels_format,GL_UNSIGNED_BYTE);
+                buffer.free();
+                            
+                //std::cout << "printando " << c.first << " - " << std::endl; 
+               // texture.set_cubemap_image(c.first,buffer.buffer,buffer.width,buffer.height,channels_format,GL_UNSIGNED_BYTE);
+
+            }
+            texture.configure(smoth,mipmap,clamp);
+            texture.unbind();
+            return texture;
+        }   
     };
 
     class Shader {
@@ -398,6 +572,7 @@ namespace DGL {
         }
     };
     class EBO : public Buffer {
+        public:
         EBO(GLuint id) : Buffer(id,GL_ELEMENT_ARRAY_BUFFER) {}
         EBO() : Buffer() {id=0; type=GL_ELEMENT_ARRAY_BUFFER; size=0;}
         inline void draw(GLsizei count=0,GLuint mode=GL_TRIANGLES,void* first=0) {
@@ -406,76 +581,48 @@ namespace DGL {
         }
     }; 
 
-   class CPUTextureBuffer {
+    class GPUMesh {
         public:
-            uint8_t *buffer=NULL;
-            size_t buffer_size;
-            uint32_t width=0;
-            uint32_t height=0;
-            uint32_t channels=1;
-            
-        static inline CPUTextureBuffer FROM_POINTER(uint32_t width, uint32_t height, uint32_t channels,void *pointer) {
-          CPUTextureBuffer t(width,height,channels);
-          t.buffer=(uint8_t*)pointer;
-          return t;
+        VBO vbo;
+        EBO ebo;
+        bool has_ebo=false; 
+        GPUMesh(VBO v, EBO e) {
+            vbo=v;
+            ebo=e;
+            has_ebo=true;
         }
-
-        inline size_t get_pixel_index(uint32_t x,uint32_t y,uint32_t channel=0) {
-            if(x>width) { err_printf("ERRO, acessando pixel além da largura \n");  return 0; }
-            if(y>height) { err_printf("ERRO, acessando pixel além da altura \n");  return 0;  }
-            auto i=((y * width + x)*channels)+channel;
-            if(i>buffer_size) {
-                err_printf("ERRO, pesquisando indice de pixel além do limite do array \n");  return 0; 
-            }
-            return i;
-
+        GPUMesh() {  }
+        bool is_valid() {
+            return vbo.is_buffer() && (has_ebo ? ebo.is_buffer() : true);
         }
-        inline Color get_pixel_color(uint32_t x,uint32_t y) { 
-            size_t i=get_pixel_index(x,y);
-            uint8_t alpha=255;
-            if(channels==4) alpha=buffer[i+3]; 
-            return Color::FROM_BYTE_COMPONENTS(buffer[i+0],buffer[i+1],buffer[i+2],alpha);
+        bool delete_ebo() {
+            if(!has_ebo) return false;
+            ebo.delete_buffer();
+            has_ebo=false;
+            return true;
         }
-        inline void put_pixel_color(uint32_t x,uint32_t y,Color color) {
-            size_t i=get_pixel_index(x,y);
-            auto c=color.to_bytes();
-            buffer[i+0]=c.r; buffer[i+1]=c.g; buffer[i+2]=c.b;
-            if(channels==4) buffer[i+3]=c.a;
+        void delete_buffers() {
+            ebo.delete_buffer();
+            if(has_ebo) vbo.delete_buffer();
         }
-        inline void put_pixel_color(uint32_t x,uint32_t y,uint8_t v) { 
-            buffer[get_pixel_index(x,y)]=v;
-        }
-        inline void free() {
-            if(buffer) delete [] buffer;
-            buffer=NULL;
-        }
-        CPUTextureBuffer() { }
-        CPUTextureBuffer(uint32_t width,uint32_t height,uint32_t channels=4) {
-            this->width=width;
-            this->height=height;
-            this->channels=channels;
-            this->buffer_size=width*height*channels;
-        }
-        inline void alloc() { 
-            buffer_size=width*height*channels;
-            buffer=new uint8_t[buffer_size];
-            memset(buffer,255,buffer_size);
-        }
-        inline Texture send_to_gpu(bool smoth=false,bool mipmap=false, bool clamp=false) { 
-            log_printf("Sending CPUTextureBuffer to gpu...\n");
-            GLuint channels_format=GL_LUMINANCE;
-            if(channels==3) channels_format=GL_RGB;
-            if(channels==4) channels_format=GL_RGBA;
-
-            Texture texture;
-            texture.create();
-            texture.bind();
-            texture.set_image(buffer,width,height,channels_format,GL_UNSIGNED_BYTE);
-            texture.configure(smoth,mipmap,clamp);
-            texture.unbind();
-            return texture;
+        static inline GPUMesh FROM_VBO(VBO v) {
+            GPUMesh mesh= GPUMesh(v, EBO());
+            mesh.has_ebo=false;
+            return mesh;
         } 
-   };
+        void draw(GLuint mode=GL_TRIANGLES,void* first=0,GLsizei count=0) {
+            vbo.bind();
+            if(has_ebo)  {
+                ebo.bind();
+                ebo.draw(count,mode,first);
+                ebo.unbind();
+            } else {
+                vbo.draw();
+            }
+            vbo.unbind();
+        }
+    };
+
 }
 
 #endif
